fdae35698767dc047dc183f66dde179e3580ae12
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 00:27:30 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,16 +1,18 @@
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
-  phrase
 end
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
-  status
 end
 
 def link
-  
+  phrase = "It's Dangerous To Go Alone! Take This"
+  puts phrase
 end
 
+def any_phrase
+  
+end
